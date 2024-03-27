c125e184a2762143fb8322ae800babc046318cba
Dmitrii Bazhin <me@lemonbro.ru>
2024-03-27 12:37:23 +0500

    change readme

---
+++ Employee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace REA_OOP_2
+{
+    internal class Employee : Person
+    {
+    }
+}
---
+++ Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace REA_OOP_2
+{
+    internal class Person
+    {
+        protected string _firstname;
+        protected string _lastname;
+        protected int _age;
+    }
+}
