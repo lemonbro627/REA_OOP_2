76e145043548289f9b06b5e6a1cfa3daf7a6e646
Dmitrii Bazhin <me@lemonbro.ru>
2024-04-01 22:00:18 +0500

    move sorts to main class file

--- Person.cs
+++ Person.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -73,6 +74,54 @@ namespace REA_OOP_2
 
             if (this.Age > tmp.Age) { return 1; }
             if (this.Age < tmp.Age) { return -1; }
+            return 0;
+        }
+    }
+
+    public class SortByFirstName : IComparer
+    {
+        int IComparer.Compare(object x, object y)
+        {
+            Person p1 = (Person)x;
+            Person p2 = (Person)y;
+
+            return String.Compare(p1.Firstname, p2.Firstname);
+        }
+    }
+
+    public class SortByLastName : IComparer
+    {
+        int IComparer.Compare(object x, object y)
+        {
+            Person p1 = (Person)x;
+            Person p2 = (Person)y;
+
+            return String.Compare(p1.Lastname, p2.Lastname);
+        }
+    }
+
+    public class SortByAge : IComparer
+    {
+        int IComparer.Compare(object x, object y)
+        {
+            Person p1 = (Person)x;
+            Person p2 = (Person)y;
+
+            if (p1.Age < p2.Age) { return -1; }
+            if (p1.Age > p2.Age) { return 1; }
+            return 0;
+        }
+    }
+
+    public class SortBySex : IComparer
+    {
+        int IComparer.Compare(object x, object y)
+        {
+            Person p1 = (Person)x;
+            Person p2 = (Person)y;
+
+            if (p1.Sex == false && p2.Sex == true) { return -1; }
+            if (p1.Sex == true && p2.Sex == false) { return 1; }
             return 0;
         }
     }
